2169fbb0bd8b7db906385e20d8b3078840773e48
netanel <netamal3134@gmail.com>
2026-06-02 20:52:23 +0300

    Stop tracking local Vercel build output with Windows paths

--- anniversary-cards/.vercel/output/static/_app/immutable/chunks/BeuPggK_.js
+++
@@ -1,1 +0,0 @@
-import{ap as s,an as l}from"./QebGMMPN.js";function u(i,e,r,n,d){var f;s&&l();var a=(f=e.$$slots)==null?void 0:f[r],t=!1;a===!0&&(a=e.children,t=!0),a===void 0||a(i,t?()=>n:n)}export{u as s};
